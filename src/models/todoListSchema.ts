82b27389d7cd68b7e330cded13631cbdd7ca4681
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-07 18:53:04 +0200

    add todo List Schema

---
+++ src/models/todoListSchema.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { AutoIncrementID } from '@typegoose/auto-increment';
+
+const schema = new mongoose.Schema({
+    _id: Number,
+    title: { type: String, required: true, unique: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    priority: { type: String, enum: ['high', 'medium', 'low'] },
+    status: { type: String, enum: ['in_progress', 'under_review', 'rework', 'completed'], default: 'in_progress' },
+    start_date: { type: String, required: false, default: null, select: false },
+    end_date: { type: String, required: false, default: null, select: false },
+    user: { type: Number, ref: 'users' }
+}, { timestamps: true });
+
+schema.plugin(AutoIncrementID, [{ filed: '_id' }]);
+
+export default mongoose.model('todo_lists', schema)
