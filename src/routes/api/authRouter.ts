2d8a25474a1cefa41d970c7a68fba1062da9f2d7
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-07 19:53:21 +0200

    start with auth router

--- src/routes/api/authRouter.ts
+++ src/routes/api/authRouter.ts
@@ -1,14 +1,50 @@
-import express, { Router } from 'express';
+import { Router } from 'express';
+import { login, register, getUserData, deleteUser, logout } from '../../controllers/authController';
 import { body, check } from 'express-validator';
 
-import { login } from '../../controllers/authController';
 import User from '../../models/userSchema';
-
-
+import checkTokens from '../../utilities/checkTokens';
 
 const auth: Router = Router()
 
+auth.route('')
+    .get(checkTokens, checkID(), getUserData)
+    .delete(checkTokens, checkID(), deleteUser)
 
-auth.get('', login);
+auth.post('/login', checkEmail(), login);
+auth.post('/register', checkUserData(), register);
+auth.post('/logout', checkTokens, checkID(), logout);
+
+// #=======================================================================================#
+// #			                         check function                                    #
+// #=======================================================================================#
+
+function checkID() {
+    return [
+        body("_id").isInt().withMessage('invalid Comment ID')
+    ]
+}
+
+function checkEmail() {
+    return [
+        body('email').isEmail().withMessage('invalid email')
+    ]
+}
+
+function checkUserData() {
+    return [
+        body('name').isAlpha().withMessage('invalid name'),
+        check('email')
+            .isEmail().withMessage('invalid email')
+            .custom((userEmail: String) => {
+                return User.findOne({ email: userEmail })
+                    .then((data) => {
+                        if (data && data.email != userEmail)
+                            return Promise.reject('Email already exit')
+                    })
+            }),
+        body('password').isStrongPassword().withMessage('Password Must contain at least 1 characters(upper and lower),numbers,special characters'),
+    ]
+}
 
 export default auth;
