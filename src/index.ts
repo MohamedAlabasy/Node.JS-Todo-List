98d600076ad7fae25fd96ff844ba5f9788d302a8
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-07 15:35:41 +0200

    add middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,32 @@
 import express from 'express';
+
+import morganMiddleware from './middleware/morganMiddleware';
+import headerAccessMiddleware from './middleware/headerAccessMiddleware';
+import notFoundMiddleware from './middleware/notFoundMiddleware';
+import errorMiddleware from './middleware/errorMiddleware';
+
 const app = express();
 
 app.listen(8888, () => {
     console.log(`App Run at http://localhost:8888`);
 });
 
-app.use((request: express.Request, response: express.Response, next: express.NextFunction) => {
+app.get('/',(request: express.Request, response: express.Response, next: express.NextFunction) => {
     response.send('hello')
 })
+
+// #=======================================================================================#
+// #			                     add header or use cors                                #
+// #=======================================================================================#
+app.use(headerAccessMiddleware);
+// #=======================================================================================#
+// #			                        not Found middleware                               #
+// #=======================================================================================#
+app.use(notFoundMiddleware);
+// #=======================================================================================#
+// #			                           middleware                                      #
+// #=======================================================================================#
+app.use(errorMiddleware);
+
 
 export default app;
