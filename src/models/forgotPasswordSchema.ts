fe8bd511e45083c055701774d2f4451ad9eb012f
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-07-11 00:47:02 +0200

    add forgot password schema

---
+++ src/models/forgotPasswordSchema.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import { AutoIncrementID } from '@typegoose/auto-increment';
+
+const schema = new mongoose.Schema({
+    _id: Number,
+    code: { type: String, required: true, trim: true },
+    created_at: { type: Date, required: true, default: Date.now },
+    expire_at: { type: Date, required: true },
+    user: { type: Number, ref: 'users' }
+}, { timestamps: true });
+
+schema.plugin(AutoIncrementID, [{ filed: '_id' }]);
+
+export default mongoose.model('forgot_password', schema);
+
